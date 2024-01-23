779af2a7821219ca445ddecf4eabaa89ae2eb74a
MahdiHaeri <mahdihaerim@gmail.com>
2024-01-23 19:18:32 +0330

    Add gotoxy function

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <curses.h>
+#include <unistd.h> // for usleep
+#include "utils/gotoxy.h"
 
 int main() {
-    printf("Hello, World!\n");
+    initscr(); // Initialize the curses library
+    keypad(stdscr, TRUE); // Enable special keys
+    nodelay(stdscr, TRUE); // Non-blocking input
+
     return 0;
 }
