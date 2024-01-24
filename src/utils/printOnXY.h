b670cda134f1bc09a8d8c6647ac9290621c15e5c
MahdiHaeri <mahdihaerim@gmail.com>
2024-01-24 16:45:31 +0330

    Fix bug in printOnXy

--- src/utils/printOnXY.h
+++ src/utils/printOnXY.h
@@ -9,17 +9,20 @@
 #include "string.h"
 
 
-void printOnXY(int x, int y, char str[]) {
+void printOnXY(int x, int y, const char str[]) {
     int line = 0;
-    gotoxy(x, y);
+    move(y, x); // Corrected the order of parameters
+
     for (int i = 0; i < strlen(str); i++) {
         if (str[i] == '\n') {
             line++;
-            gotoxy(x, y + line);
+            move(y + line, x); // Move to the next line
         } else {
-            printf("%c", str[i]);
+            addch(str[i]);
         }
     }
+
+    refresh();
 }
 
 #endif //FP_402_DEMO_PROJECT_PRINTONXY_H
