a81175b78b68af6a66321c061965163717c98ea1
MahdiHaeri <mahdihaerim@gmail.com>
2024-01-25 05:54:50 +0330

    Fix bug in printOnXy.h

--- src/utils/printOnXY.h
+++ src/utils/printOnXY.h
@@ -13,22 +13,21 @@ void printOnXY(int x, int y, const char str[]) {
     int column = 0;
     int row = 0;
     int max_x, max_y;
+    int min_x, min_y;
 
     getmaxyx(stdscr, max_y, max_x); // Get the dimensions of the window
+    getbegyx(stdscr, min_y, min_x); // Get the starting position of the window
 
     move(y, x); // Corrected the order of parameters
 
     for (int i = 0; i < strlen(str); i++) {
-        if (y + row >= max_y) {
-            break;
-        }
-        if (x + column >= max_x) {
-            continue;
-        }
         if (str[i] == '\n') {
             row++;
-            move(y + row, x); // Move to the next line
             column = 0;
+            move(y + row, x); // Move to the next line
+        } else if (x + column >= max_x || x + column < min_x || y + row >= max_y || y + row < min_y) {
+            column++;
+            continue;
         } else {
             move(y + row, x + column); // Move to the next column
             addch(str[i]);
